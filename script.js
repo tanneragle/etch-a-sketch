2eeabe136dfdab1577597977ea1cbc8ea6382b79
tanneragle <tanneragle@gmail.com>
2025-01-28 17:34:23 -0500

    Add ability to keep grid as the same amount of squares on each side

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Global Variables
 const container = document.getElementById("container");
+const containerWidth = container.offsetWidth;
 
-let gridSize = 16
+let gridSize = 10
 
-console.log(gridSize ** 2);
+const squareSize = containerWidth / gridSize;
 
 // Loop for generating grid
-for(let i = 0; i <= `${gridSize ** 2}`; i++) {
+for(let i = 0; i < `${gridSize ** 2}`; i++) {
     const divBox = document.createElement("div");
     divBox.setAttribute("id", "divBox");
-    divBox.style.width = "100%";
+    divBox.style.width = `${squareSize}px`;
+    divBox.style.height = `${squareSize}px`;
     divBox.style.aspectRatio = "1 / 1"; 
     divBox.style.boxSizing = "border-box";
     divBox.style.border = "1px solid black";
